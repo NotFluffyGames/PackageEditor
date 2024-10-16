1b1b84f2efe316101537901a50ee480bb6dc92a4
Gal Ben Naim <35352105+GalBNx@users.noreply.github.com>
2024-10-16 10:57:00 +0300

    Fixed DB serialization

--- Scripts/PackageEditorDB.cs
+++ Scripts/PackageEditorDB.cs
@@ -2,18 +2,20 @@
 
 using System.Collections.Generic;
 using System.IO;
-using UnityEditor.PackageManager;
-using UnityEngine;
+using System.Linq;
+using UnityEditor;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
 namespace NotFluffy.PackageEditor
 {
+	[InitializeOnLoad]
 	public static class PackageEditorDB
 	{
 		private const string DATABASE_NAME = "PackageEditorDB.json";
 
 		private static readonly Dictionary<string, string> Entries;
 
-		#region Utilities
+		public static int Count => Entries.Count;
 
 		static PackageEditorDB()
 		{
@@ -28,7 +30,8 @@ namespace NotFluffy.PackageEditor
 				return new();
 			
 			var contents = File.ReadAllText(filepath);
-			return JsonUtility.FromJson<Dictionary<string, string>>(contents);
+			var result = FromString(contents);
+			return result;
 		}
 
 		public static bool TryGetUrl(PackageInfo packageInfo, out string url) => Entries.TryGetValue(packageInfo.name, out url);
@@ -54,13 +57,30 @@ namespace NotFluffy.PackageEditor
 		private static void Flush()
 		{
 			var filepath = GetDatabasePath();
-			var contents = JsonUtility.ToJson(Entries, true);
+			var contents = ToString(Entries);
 			File.WriteAllText(filepath, contents);
 		}
-		
-		private static string GetDatabasePath() => Path.Combine(Path.GetFullPath("Packages"), DATABASE_NAME);
 
-		#endregion
+		private static string ToString(Dictionary<string, string> dict)
+		{
+			return string.Join("\n", dict.Select(pair => $"{pair.Key}: {pair.Value}"));
+		}
+
+		private static Dictionary<string, string> FromString(string str)
+		{
+			var result = new Dictionary<string, string>();
+			
+			var pairs = str.Split("\n");
+			foreach (var pair in pairs)
+			{
+				var pairValues = pair.Split(": ");
+				result.Add(pairValues[0], pairValues[1]);
+			}
+
+			return result;
+		} 
+
+		private static string GetDatabasePath() => Path.Combine(Path.GetFullPath("Packages"), DATABASE_NAME);
 	}
 }
 
